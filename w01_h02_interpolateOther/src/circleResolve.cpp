155e0b7f0e1f0554fffa9841f2d9c58f8091525b
Tyler Henry <henrt555@newschool.edu>
2015-02-10 17:19:33 -0500

    added week 1 hw / fixed extra directories

--- w01_h02_interpolateOther/src/circleResolve.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  circleResolve.cpp
-//  w01_h02_interpolateOther
-//
-//  Created by Tyler Henry on 2/10/15.
-//
-//
-
-#include "circleResolve.h"
--- w01_h02_interpolateStuff/src/circleResolve.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  circleResolve.cpp
-//  w01_h02_interpolateStuff
-//
-//  Created by Tyler Henry on 2/10/15.
-//
-//
-
-#include "circleResolve.h"
