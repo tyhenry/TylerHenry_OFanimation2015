01d2f9f544f5d5bdc5bf70d5d6216173152b8f9c
Tyler Henry <henrt555@newschool.edu>
2015-04-07 18:26:46 -0400

    added w07 hw

---
+++ w07_h02_pertyFlowField/src/Particle.cpp
@@ -0,0 +1,72 @@
+//
+//  Particle.cpp
+//  w10_03_flowField
+//
+//  Created by Tyler Henry on 3/31/15.
+//
+//
+
+#include "Particle.h"
+
+void Particle::setup(){
+    
+    pos.x = ofRandomWidth();
+    pos.y = ofRandomHeight();
+    oldPos.set(pos.x,pos.y);
+    
+}
+
+void Particle::resetForces(){
+    acc *= 0;
+    
+}
+
+void Particle::applyForce(ofVec2f force){
+    acc += force;
+    
+}
+
+void Particle::applyDampingForce(float damp){
+    acc -= vel.getNormalized() * damp;
+    
+}
+
+void Particle::update(){
+    oldPos.set(pos.x,pos.y);
+    
+    vel += acc;
+    pos += vel;
+    
+    if (pos.x < 0){
+        pos.x = 0;
+        vel.set(-vel.x, vel.y);
+    }
+    if (pos.x > ofGetWidth()){
+        pos.x = ofGetWidth();
+        vel.set(-vel.x, vel.y);
+    }
+    if (pos.y < 0){
+        pos.y = 0;
+        vel.set(vel.x, -vel.y);
+    }
+    if (pos.y > ofGetHeight()){
+        pos.x = ofGetHeight();
+        vel.set(vel.x, -vel.y);
+    }
+    
+    
+}
+
+void Particle::draw(){
+    ofPushStyle();
+    
+        ofVec2f hueVec = vel.limit(20); //limit length
+        float hue = ofMap(hueVec.length(),0,15, 180,255);
+        ofColor c = ofColor::fromHsb(hue,255,255);
+        ofSetColor(c);
+    
+        ofLine(pos.x,pos.y,oldPos.x,oldPos.y);
+    
+    ofPopStyle();
+    
+}
---
+++ w07_h02_pertyFlowField/src/Particle.h
@@ -0,0 +1,24 @@
+//
+//  Particle.h
+//  w10_03_flowField
+//
+//  Created by Tyler Henry on 3/31/15.
+//
+//
+
+#pragma once
+#include "ofMain.h"
+
+class Particle {
+public:
+    
+    void setup();
+    void resetForces();
+    void applyForce(ofVec2f force);
+    void applyDampingForce(float damp);
+    void update();
+    void draw();
+    
+    ofVec2f pos, vel, acc, oldPos;
+    
+};
